fdf38974840868f239b618d7f8b385986114389a
eitsupi <ts1s1andn@gmail.com>
2023-11-15 14:30:15 +0000

    refactor: remove unused lines and add a comment

--- R/dbClearResult_Result.R
+++ R/dbClearResult_Result.R
@@ -4,10 +4,8 @@
 dbClearResult_polarssql_result <- function(res, ...) {
   res@env$rows_fetched <- 0
   res@env$rows_affected <- 0
-
-  res@statement <- NA_character_
   res@env$query_plan <- pl$LazyFrame()
-
+  # TODO: Remove resultset by https://github.com/pola-rs/r-polars/pull/504
   res@env$resultset <- res@env$query_plan$collect()
 
   invisible(TRUE)
