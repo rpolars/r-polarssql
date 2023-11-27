0542baa93e741a8b9900196529a039f72a570316
eitsupi <ts1s1andn@gmail.com>
2023-11-27 15:15:52 +0000

    docs: fix examples of dbplyr functions

--- R/pkg-dbplyr.R
+++ R/pkg-dbplyr.R
@@ -17,7 +17,7 @@
 #'   select(contains("c")) |>
 #'   head(n = 3)
 #'
-#' # Actual polarssql connection shows the Polars naive plan.
+#' # Actual polarssql connection shows the Polars naive plan (LazyFrame).
 #' tbl_polarssql(mtcars) |>
 #'   filter(cyl == 4) |>
 #'   arrange(desc(mpg)) |>
@@ -25,7 +25,7 @@
 #'   head(n = 3)
 #'
 #' # Unlike other dbplyr backends, `compute` has a special behavior.
-#' # It returns a polars DataFrame or LazyFrame.
+#' # It returns a polars DataFrame.
 #' tbl_polarssql(mtcars) |>
 #'   filter(cyl == 4) |>
 #'   arrange(desc(mpg)) |>
@@ -152,7 +152,7 @@ print.tbl_polarssql_connection <- function(x, ...) {
   if (inherits(x$src$con, "TestConnection")) {
     NextMethod("print")
   } else {
-    print(compute.tbl_polarssql_connection(x, eager = FALSE))
+    print(as_polars_lf(x))
     invisible(x)
   }
 }
